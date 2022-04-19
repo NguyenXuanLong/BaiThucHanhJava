87e26418b163af17f0fd5623e5e6738c90a3812e
NguyenXuanLong <longnguyenxuan2000@gmail.com>
2022-04-19 14:55:43 +0700

    BaiThucHanh1904

---
+++ BaiThucHanh1904/src/Main.java
@@ -0,0 +1,20 @@
+import People.Person;
+import People.Student;
+import People.OnlineStudent;
+import People.OfflineStudent;
+import People.Customer;
+import People.VIPCustomer;
+import People.Employee;
+import People.FullTimeEmployee;
+import People.PartTimeEmployee;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Student student = new Student();
+        
+    }
+
+}
+
+
